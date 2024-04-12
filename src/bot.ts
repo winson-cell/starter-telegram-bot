6a27e67634dfde8aa3bb5a77df634be8c184e6c5
winson-cell <59526011+winson-cell@users.noreply.github.com>
2024-04-12 10:53:12 +0800

    adding check status

--- src/bot.ts
+++ src/bot.ts
@@ -7,9 +7,54 @@ import type { Variant as TextEffectVariant } from "./textEffects";
 
 // Create a bot using the Telegram token
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
+const addresses = ["rMkHVoVFk3vLGJgJCUjcGnjiq2jRsk4vsx", "rHLLfANXaJgM44zZTSHBpjK3nfj8v1zqg", "rJw2tbTmYaGTpQFHYNY2ZVQVkiJ6m6hgPd", "rh61B9Fc9B1fGLhux4DFDejrXh9vYjxqrw", "rEF4TADNwLy4wg73mc72Fu9w8j8NwKG1xD", "rDjCaAWNJNSzBAqVU3xmuLTUmA6uQBzDhU","rnmFKEUUSV7f3tesgKE3ETWMs83Bm9Vw9K","rG9jC9zH4gaSYAxtstEiMmc6UKEKGAyfM","rhp2TbZzHUK2TvFh2D5GhqJLM9S6NSHzsJ","rsQKVUDihMp2HHV9ZeRh1rTMd8jNwPKjEm","rfGPGXDi4KqwiDNUwqieK3pZH9DNeD21zG","rNRMPZkawUTCBjhVcDwftGGMp2f67GjaZF","rJUncLkASRPU11KarYxWn43vJWVv1MBC7a","rGLDRDBVDEUbQYrZGosDShfmcjs2hChd8j","rf9Jzd3zHs6sKr7gQSsHiro7PfmwRnrz94","rHPStZVvY8vfHnmSAz42YS2nCokFT6Lx64","rKs52FV88bi4aPKTNCE8J8mkfCEKA1FjNC","rMPXb5ACRmZjHxsyJoSRSDtwqk794gZ5i7","rxNGVH7UmFaDny9JAi2V5NAPJNrkDRMFA","rEkX9U8hsmNh49bps5LS29PuuC9J3Tu1TV","rscubpHLuqhKSyPJW1en8L6wU3uV4hY6PV","rJrXD3SvMR4tSrjk7w1C5HirEcdsjUUrbp","rBwFA5hFFDcKntehMxu4PzT48zvjwMqQos","rf1oYmmAYNv6L1T6hP32SNg9y7Dvdj7rsn","rnC9wViUaGxuSimhjgs3muHGihdjazwsh3","rfDC19ArUcmJko1bQBxfKkZ6ft53yqw6yk"]
+const evernode = require("evernode-js-client");
 
 // Handle the /yo command to greet the user
 bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
+
+bot.command("start", async (ctx) => {main(ctx.msgId)});
+async function main(chatId) {
+  await bot.api.sendMessage(chatId, "Hi!");
+}
+// async function main(chatId) {
+//   let active = 0
+//   let not_active = 0
+//   try {
+//     // Use a singleton xrplApi for all tests.
+//     await evernode.Defaults.useNetwork('mainnet');
+//     const xrplApi = new evernode.XrplApi(null, { autoReconnect: false });
+//     evernode.Defaults.set({
+//       xrplApi: xrplApi,
+//       useCentralizedRegistry: true // Conent to use centralized registry functions.
+//     });
+//     await xrplApi.connect();
+//     const governorClient = await evernode.HookClientFactory.create(evernode.HookTypes.governor);
+//     await governorClient.connect();
+//     bot.sendMessage(chatId,"Connected checking host");
+//     var total = 0
+//     var i = 0
+//     for (const address of addresses) {
+//       await governorClient.getHostInfo(address).then((result) => {
+//         i = i + 1
+//         if (result["active"] == false){
+//           bot.sendMessage(chatId,"something went wrong pls check" + address)
+//           not_active = not_active + 1
+//         }
+//         else{
+//           active = active + 1
+//         }
+//       })
+//         .catch((error) => {
+//           console.error(error); // This will be called if the promise is rejected
+//         });
+//     }
+//     } catch (e) {
+//       console.log(e)
+//     }
+//     bot.sendMessage(chatId,"active host : " + active.toString())
+//     bot.sendMessage(chatId,"not active host : " + not_active.toString())
+//   }
 
 // Handle the /effect command to apply text effects using an inline keyboard
 type Effect = { code: TextEffectVariant; label: string };
