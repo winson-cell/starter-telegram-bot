5137772dddcde8cadd757c5b6a867fbfb4c9647b
winson-cell <59526011+winson-cell@users.noreply.github.com>
2024-04-23 13:58:28 +0800

    added running job every hour

--- src/bot.ts
+++ src/bot.ts
@@ -1,22 +1,99 @@
 import { Bot, InlineKeyboard, webhookCallback } from "grammy";
-import { add, chunk } from "lodash";
 import express from "express";
-import { applyTextEffect, Variant } from "./textEffects";
+const { MongoClient } = require('mongodb');
 
-import type { Variant as TextEffectVariant } from "./textEffects";
 
 // Create a bot using the Telegram token
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 const addresses_boss = ["rMkHVoVFk3vLGJgJCUjcGnjiq2jRsk4vsx", "rHLLfANXaJgM44zZTSHBpjK3nfj8v1zqg", "rJw2tbTmYaGTpQFHYNY2ZVQVkiJ6m6hgPd", "rh61B9Fc9B1fGLhux4DFDejrXh9vYjxqrw", "rEF4TADNwLy4wg73mc72Fu9w8j8NwKG1xD", "rDjCaAWNJNSzBAqVU3xmuLTUmA6uQBzDhU","rnmFKEUUSV7f3tesgKE3ETWMs83Bm9Vw9K","rG9jC9zH4gaSYAxtstEiMmc6UKEKGAyfM","rhp2TbZzHUK2TvFh2D5GhqJLM9S6NSHzsJ","rsQKVUDihMp2HHV9ZeRh1rTMd8jNwPKjEm","rfGPGXDi4KqwiDNUwqieK3pZH9DNeD21zG","rNRMPZkawUTCBjhVcDwftGGMp2f67GjaZF","rJUncLkASRPU11KarYxWn43vJWVv1MBC7a","rGLDRDBVDEUbQYrZGosDShfmcjs2hChd8j","rf9Jzd3zHs6sKr7gQSsHiro7PfmwRnrz94","rHPStZVvY8vfHnmSAz42YS2nCokFT6Lx64","rKs52FV88bi4aPKTNCE8J8mkfCEKA1FjNC","rMPXb5ACRmZjHxsyJoSRSDtwqk794gZ5i7","rxNGVH7UmFaDny9JAi2V5NAPJNrkDRMFA","rEkX9U8hsmNh49bps5LS29PuuC9J3Tu1TV","rscubpHLuqhKSyPJW1en8L6wU3uV4hY6PV","rJrXD3SvMR4tSrjk7w1C5HirEcdsjUUrbp","rBwFA5hFFDcKntehMxu4PzT48zvjwMqQos","rf1oYmmAYNv6L1T6hP32SNg9y7Dvdj7rsn","rnC9wViUaGxuSimhjgs3muHGihdjazwsh3","rfDC19ArUcmJko1bQBxfKkZ6ft53yqw6yk"]
 const evernode = require("evernode-js-client");
+const mongo_uri = "mongodb://mongo:fm8Ty9C2dGL16hz543lEauKU7iODA0YW@hnd1.clusters.zeabur.com:30451";
+const client = new MongoClient(mongo_uri);
+
+const schedule = require("node-schedule");
+let job:any;
+// job = schedule.scheduleJob('1 */1 * * *', () => {
+// bot.command("start1", async (ctx) => {
+//     job = schedule.scheduleJob('*/5 * * * * *', () => {
+//         console.log("hello")
+//         ctx.reply("Connecting to Server");
+//     });
+// });
+
+bot.command("stop_job", async (ctx) => {
+  if (job) {
+        job.cancel()
+        console.log("cancelled job")
+    }
+});
+
 
 // Handle the /yo command to greet the user
 bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
+
+bot.command("check_status_boss_job", async (ctx) => {
+  await ctx.reply("Running Job Every Hour");
+  job = schedule.scheduleJob('1 */1 * * *', () => {
+    ctx.reply("Connecting to Server");
+    main(ctx, addresses_boss)
+  })
+});
 
 bot.command("check_status_boss", async (ctx) => {
   await ctx.reply("Connecting to Server");
   main(ctx, addresses_boss)
 });
+
+bot.command("add_account", async (ctx :any) => {
+  await ctx.reply("adding account");
+  // splitting into an array
+  let accounts = ctx.message['text'].split(" ")
+  accounts = accounts[1].split(",")
+  for(let i=0; i<=accounts.length - 1; i++) {
+    accounts[i] = accounts[i].split(' ').join('')
+  }
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      // await listDatabases(client);
+      const db = client.db("Evernode");
+
+      await db.collection('Account').insertOne({
+        username: ctx.message["chat"]["username"],
+        accounts: accounts,
+      });
+
+  } catch (e) {
+        console.error(e);
+  } finally {
+      // Close the connection to the MongoDB cluster
+      await client.close();
+      await ctx.reply("done adding account");
+  }
+
+});
+
+bot.command("check_account", async (ctx :any) => {
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+    const db = client.db("Evernode");
+    const accounts = db.collection("accounts");
+    console.log(ctx.message["chat"]["username"])
+    const query = { username: "Winson2000" };
+    const result = await accounts.findOne(query)
+    console.log(result)
+    } catch (e) {
+          console.error(e);
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+        await ctx.reply("Found account");
+    }
+})
 
 bot.command("check_address", async (ctx :any) => {
   // console.log(ctx.message)
@@ -32,8 +109,6 @@ bot.command("check_address", async (ctx :any) => {
   // console.log(message_address)
   await ctx.reply("Connecting to Server");
   main(ctx, message_address)
-  // await ctx.reply(`${ctx.message}`);
-  // main(ctx)
 });
 
 
@@ -106,7 +181,7 @@ const replyWithIntro = (ctx: any) =>
     parse_mode: "HTML",
   });
 
-bot.command("start", replyWithIntro);
+// bot.command("start", replyWithIntro);
 bot.on("message", replyWithIntro);
 
 // Start the server
